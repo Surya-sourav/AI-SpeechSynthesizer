796ca247c2f25a3c574a4b2c5e9d52cd6a4724d4
Surya <surya.aimlx@gmail.com>
2024-09-05 17:16:47 +0530

    Testing_phase1

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import soundfile as sf  # To save the output as a wav file
 
 # Step 1: Load the model configuration
 config = XttsConfig()
-config.load_json("/path/to/xtts/config.json")
+config.load_json("/home/omtrimoco/tts_project/Assets/config.json")
 
 # Step 2: Initialize the model
 model = Xtts.init_from_config(config)
 
 # Step 3: Load the pre-trained weights
-model.load_checkpoint(config, checkpoint_dir="/path/to/xtts/", eval=True)
+model.load_checkpoint(config, checkpoint_dir="/home/omtrimoco/tts_project/Assets", eval=True)
 
 # Optional: If you have CUDA installed and want to use GPU, uncomment the line below
 # model.cuda()
@@ -19,7 +19,7 @@ model.load_checkpoint(config, checkpoint_dir="/path/to/xtts/", eval=True)
 outputs = model.synthesize(
     "It took me quite a long time to develop a voice and now that I have it I am not going to be silent.",
     config,
-    speaker_wav="/data/TTS-public/_refclips/3.wav",  # Replace with the correct path
+    speaker_wav="input.wav",  # Replace with the correct path
     gpt_cond_len=3,
     language="en",
 )
