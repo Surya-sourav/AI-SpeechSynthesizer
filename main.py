cf6617e9e2a68450f5d8ee57b1752afa7e3a7876
Surya <surya.aimlx@gmail.com>
2024-09-05 18:43:53 +0530

    Initial commit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ model.load_checkpoint(config, checkpoint_dir="/home/omtrimoco/tts_project/Assets
 
 # Step 4: Synthesize the output
 outputs = model.synthesize(
-    "It took me quite a long time to develop a voice and now that I have it I am not going to be silent.",
+    "I think i cando something great out of this project and simply i can fulfill my dream of making the podcast.ai software that would read you your desired ebooks ! moreover i will create an AI software that would read an e-book or an article for you whilst you are busy doing some other works",
     config,
     speaker_wav="input.wav",  # Replace with the correct path
     gpt_cond_len=3,
