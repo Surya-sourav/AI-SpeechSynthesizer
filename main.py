83cd58c452ca65ea2082a51b5b1473b02675a14a
Surya <surya.aimlx@gmail.com>
2024-09-09 22:18:27 +0530

    Minor Changes in main.py

--- main.py
+++ main.py
@@ -15,17 +15,49 @@ model.load_checkpoint(config, checkpoint_dir="/home/omtrimoco/tts_project/Assets
 # Optional: If you have CUDA installed and want to use GPU, uncomment the line below
 # model.cuda()
 
+f= open('data.txt','r')
+data = f.read()
+f.close()
+
+
+def createTTS(text,input_audio,output_audio):
 # Step 4: Synthesize the output
-outputs = model.synthesize(
-    "I think i cando something great out of this project and simply i can fulfill my dream of making the podcast.ai software that would read you your desired ebooks ! moreover i will create an AI software that would read an e-book or an article for you whilst you are busy doing some other works",
-    config,
-    speaker_wav="input.wav",  # Replace with the correct path
-    gpt_cond_len=3,
-    language="en",
-)
+    outputs = model.synthesize(
+        data,
+        config,
+        speaker_wav= input_audio,  # Replace with the correct path
+        gpt_cond_len=3,
+        language="en",
+    )
 
-# Step 5: Save the synthesized speech to a wav file
-output_wav = outputs['wav']
-sf.write('output.wav', output_wav, config.audio.sample_rate)
+    # Step 5: Save the synthesized speech to a wav file
+    output_wav = outputs['wav']
+    sf.write(output_audio, output_wav, config.audio.sample_rate)
 
-print("Speech synthesis complete and saved to output.wav")
+    print("Speech synthesis complete and saved to output.wav")
+
+# createTTS("How are you ?" , "input.wav", "output.wav")
+
+import sqlite3
+
+conn = sqlite3.connect('tts.db')
+
+cursor = conn.cursor()
+
+def createTable():
+    cursor.execute('''CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT ,text TEXT NOT NULL)''')
+
+def createTask(text):
+        
+    cursor.execute("INSERT INTO tasks (text) VALUES(?)",(text, ))
+    conn.commit()
+
+
+def fetchTasks():
+    cursor.execute("SELECT * FROM tasks")
+
+    rows = cursor.fetchall()
+
+print(fetchTasks())
+conn.close()
+ 
